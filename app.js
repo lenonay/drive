ecb22ab7bb68aac203b5333707ac7ab7a287262f
lenonay <dj.lama.pitito@gmail.com>
2024-12-13 15:25:16 +0000

    Proc: header html

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 import e, { json } from "express";
 import { INT, PORT } from "./config.js";
-import { DepsRouter } from "./routes/deps.js";
 
 const app = e();
 
 app.use(json());
 
+
 app.get("/favicon.ico", (_, res) => {
     res.header("Content-Type: image/png").sendFile("logo.png", { root: "./public" });
 });
+
+app.use("/public", e.static("./public"));
 
 app.get("/", (_, res) => {
     res.sendFile("./main.html", { root: "./views" });
 });
 
-app.use("/deps", DepsRouter);
+app.use("/deps", e.static("./deps"));
 
 app.listen(PORT, INT, () => {
     console.log("Server listening on:", INT, PORT);
