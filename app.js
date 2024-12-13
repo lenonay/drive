dbded7a4dda18d3da9d46713c690add3db65acc8
lenonay <dj.lama.pitito@gmail.com>
2024-12-13 14:39:24 +0000

    Proc: base html

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 import e, { json } from "express";
 import { INT, PORT } from "./config.js";
+import { DepsRouter } from "./routes/deps.js";
 
 const app = e();
 
 app.use(json());
 
-app.get("/", (_, res) => {
-    res.send("Hola");
+app.get("/favicon.ico", (_, res) => {
+    res.header("Content-Type: image/png").sendFile("logo.png", { root: "./public" });
 });
 
+app.get("/", (_, res) => {
+    res.sendFile("./main.html", { root: "./views" });
+});
+
+app.use("/deps", DepsRouter);
+
 app.listen(PORT, INT, () => {
-    console.log("Server listening on:",INT, PORT);
+    console.log("Server listening on:", INT, PORT);
 })
